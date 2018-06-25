915e746e9c68fc624cb059848226ae8fd246e2f0
Tyler Poole <tpoole@pietech.com>
2018-06-25 17:19:17 -0400

    began constructing input sidebar

--- Guraranteed_Income/src/app.ts
+++ Guraranteed_Income/src/app.ts
@@ -5,20 +5,39 @@ export class App {
   personOne = new Person();
 
   attached() {
+    var self = this;
     this.personOne.income = 50000;
     this.personOne.age = 60;
 
     var post = new HTTPPost();
     var results = post.SendData(this.personOne)
+    document.getElementById("ageInput").addEventListener("blur", function(){self.ShowElement("age","ageInput")})
+    document.getElementById("retireDateInput").addEventListener("blur", function(){self.ShowElement("retireDate","retireDateInput")})
   }
 
   OpenSidebar() {
-    document.getElementById("mySidebar").style.width = "100%";
-    document.getElementById("mySidebar").style.display = "block";
-}
+    document.getElementById("sidebar").style.width = "20%";
+    document.getElementById("sidebar").style.display = "block";
+  }
   CloseSidebar() {
-    document.getElementById("mySidebar").style.display = "none";
-}
+    document.getElementById("sidebar").style.display = "none";
+  }
+
+  HideElement(elementOne, elementTwo) {
+    var link = document.getElementById(elementOne);
+    var text = document.getElementById(elementTwo);
+    link.style.display = "none";
+    text.style.display = "block";
+    text.focus();
+  }
+
+  ShowElement(elementOne, elementTwo) {
+    var link = document.getElementById(elementOne);
+    var text = document.getElementById(elementTwo);
+    link.style.display = "block";
+    text.style.display = "none";
+  }
+
 
   results;
 }
