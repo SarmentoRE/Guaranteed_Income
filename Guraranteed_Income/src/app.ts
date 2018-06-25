2c329580f0b25b6514350d2bc9989d9254835e7b
Tyler Poole <tpoole@pietech.com>
2018-06-25 15:05:49 -0400

    removed spouse

--- Guraranteed_Income/src/app.ts
+++ Guraranteed_Income/src/app.ts
@@ -3,15 +3,22 @@ import HTTPPost from "httpPost"
 
 export class App {
   personOne = new Person();
-  personTwo = new Person();
 
   attached() {
     this.personOne.income = 50000;
     this.personOne.age = 60;
 
     var post = new HTTPPost();
-    var results = post.SendData(this.personOne, this.personTwo)
+    var results = post.SendData(this.personOne)
   }
+
+  OpenSidebar() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block";
+}
+  CloseSidebar() {
+    document.getElementById("mySidebar").style.display = "none";
+}
 
   results;
 }
