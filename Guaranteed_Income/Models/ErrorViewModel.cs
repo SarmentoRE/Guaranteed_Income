dddd36e4e67947509ca421c4734bbf31a40757ed
Austin Sarmento <asarmento@pietech.com>
2018-06-25 10:19:19 -0400

    initial commit

---
+++ Guaranteed_Income/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Guaranteed_Income.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
